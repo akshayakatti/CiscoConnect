109eea778fcf79f3142a4d266c5e1a1eb5b04bf1
Akshaya A K <38110588+akshayakatti@users.noreply.github.com>
2020-09-08 17:18:10 +0530

    Add files via upload
    Remote Cisco Connect

---
+++ CiscoDisconnect.bat
@@ -0,0 +1,45 @@
+@echo off 
+echo *******************************************************
+echo * start %~f0
+echo *******************************************************
+echo.
+echo *******************************************************
+set "LAUNCH_DIR=%~dp0"
+echo Launch_Directory : %LAUNCH_DIR%
+echo *******************************************************
+echo.
+echo.
+echo *******************************************************
+set "CURRENT_DIR=%CD%"
+echo Current_Directory : %CURRENT_DIR%
+echo *******************************************************
+echo.
+echo.
+
+echo *******************************************************
+set "VPN_CLIENT_EXE_PATH="C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client""
+echo VPN Cisco Client Executable Path : %VPN_CLIENT_EXE_PATH%
+echo *******************************************************
+echo.
+
+echo.
+echo **** Cisco AnyConnect Secure VPN Connection Connecting *************
+echo Running Command "%VPN_CLIENT_EXE_PATH%\vpncli.exe disconnect"
+echo.
+%VPN_CLIENT_EXE_PATH%\vpncli.exe disconnect
+echo.
+echo %ERRORLEVEL%
+IF "%ERRORLEVEL%" == "0" ( 
+	echo ===============================================
+	echo **** Cisco Client Disconnect sucsessfully ***
+	echo ===============================================
+	TIMEOUT /T 6
+	exit /b 0
+) ELSE (
+  echo ===================================
+  echo **** Cisco Client Disconnect Failed  ***
+  echo ===================================
+  pause
+  exit /b 1
+)
+pause
---
+++ CiscoLoginInfo.bat
@@ -0,0 +1,3 @@
+connect "APAC - Bangalore"
+uidr2685@cw01.contiwan.com
+Ganesha@99
